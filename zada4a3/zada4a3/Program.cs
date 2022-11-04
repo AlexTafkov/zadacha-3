b5a2e67e5b05d3d1d3114cb8e91d8d0a5cd92ccc
AlexTafkov <76816466+AlexTafkov@users.noreply.github.com>
2022-11-04 18:42:37 +0200

    Add files via upload

---
+++ zada4a3/zada4a3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zada4a3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, z, result;
+
+            x = double.Parse(Console.ReadLine());
+            y = double.Parse(Console.ReadLine());
+            z = double.Parse(Console.ReadLine());
+
+            result = (x + y) / (z + z / x + z / y - x);
+
+            Console.WriteLine("Резултат= " + result);
+            Console.WriteLine("Натиснете произволен клавиш. . .");
+        }
+    }
+}
